92c1868458c4d0a326ac9964ff30fff0cc43f72a
Ahmed Mahdy <mo.a9tay@gmail.com>
2022-08-11 02:29:34 +0200

    [UI_FEATURE] seperated button from the  and made it as a UI component

---
+++ lib/presentation/ui_components/default_filled_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/constants/theme.dart';
+
+class DefaultFilledButton extends StatelessWidget {
+  const DefaultFilledButton({
+    Key? key,
+    required this.child,
+    required this.onPressed,
+  }) : super(key: key);
+  final Widget child;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 315.w,
+      height: 56.h,
+      child: TextButton(
+          style: TextButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.r)),
+              backgroundColor: themePrimaryColor,
+              primary: Colors.white,
+              padding: const EdgeInsets.all(10),
+              textStyle: TextStyle(fontSize: 18.r)),
+          onPressed: onPressed,
+          child: child),
+    );
+  }
+}
